71b787c224a800104ba627f9a34df952fcc68418
ABradbury385 <abradbury13231@gmail.com>
2022-01-26 23:13:33 -0500

    Fixed sticking to walls

--- GGJ Duality/Assets/MetroidvaniaController/Scripts/Player/PlayerMovement.cs
+++ GGJ Duality/Assets/MetroidvaniaController/Scripts/Player/PlayerMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public CharacterController2D controller;
+	public PhysicsMaterial2D slippery;
+	public PhysicsMaterial2D sticky;
+	private Rigidbody2D rb;
 	public Animator animator;
 
 	public float runSpeed = 40f;
@@ -13,14 +16,31 @@ public class PlayerMovement : MonoBehaviour {
 	bool jump = false;
 	bool dash = false;
 
-	//bool dashAxis = false;
-	
-	// Update is called once per frame
-	void Update () {
+    //bool dashAxis = false;
+
+    private void Start()
+    {
+		rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
 
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
 		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+		//make sure you don't slide on slopes
+		if (horizontalMove == 0.0f && rb.velocity.y == 0.0f)
+		{
+			rb.sharedMaterial = sticky;
+		}
+
+		//don't stick to platforms
+		else
+		{
+			rb.sharedMaterial = slippery;
+		}
 
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
